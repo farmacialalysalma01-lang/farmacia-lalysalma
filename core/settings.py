d9c82ce9d15092ef9c468076df4e9e07731cc84a
farmacialalysalma01-lang <farmacialalysalma01@gmail.com>
2025-12-18 18:18:52 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -55,16 +55,6 @@ DATABASES = {
     }
 }
 
-if os.environ.get("RENDER") == "true":
-    from django.contrib.auth.models import User
-    try:
-        User.objects.get(username="admin")
-    except User.DoesNotExist:
-        User.objects.create_superuser(
-            username="admin",
-            email="farmacialalysalma01@gmail.com",
-            password="Admin@123"
-        
 LANGUAGE_CODE = 'pt-pt'
 
 TIME_ZONE = 'Africa/Maputo'
