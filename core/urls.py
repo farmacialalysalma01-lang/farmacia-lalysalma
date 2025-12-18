eca48ee907e535f5b5cefa8360aac66d24c48e31
farmacialalysalma01-lang <farmacialalysalma01@gmail.com>
2025-12-18 12:35:48 +0200

    Update URL patterns to include farmacia.urls

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from farmacia.views import home
+from django.urls import path, include
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', include('farmacia.urls')),
     path('admin/', admin.site.urls),
 ]
