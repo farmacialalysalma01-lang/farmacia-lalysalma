5cbcface6e6051da37088e3a80c5d73d51a886e9
farmacialalysalma01-lang <farmacialalysalma01@gmail.com>
2025-12-20 00:18:07 +0200

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -14,3 +14,10 @@ def criar_admin(request):
         password="Admin@123"
     )
     return HttpResponse("✅ Admin criado com sucesso")
+    
+from django.core.management import call_command
+from django.http import HttpResponse
+
+def executar_migracoes(request):
+    call_command("migrate")
+    return HttpResponse("Migrações executadas com sucesso")
