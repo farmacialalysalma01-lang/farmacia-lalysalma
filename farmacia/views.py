5b01835c516cffe4d4730bcc3517023b0de50f24
farmacialalysalma01-lang <farmacialalysalma01@gmail.com>
2025-12-20 01:09:13 +0200

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -1,23 +1,14 @@
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
-def home(request):
-    return HttpResponse("Sistema Funcionando – Farmácia Lalysalma")
-
 def criar_admin(request):
-    if User.objects.filter(username="admin").exists():
-        return HttpResponse("⚠️ Admin já existe")
+    if User.objects.filter(username='admin').exists():
+        return HttpResponse("Admin já existe")
 
     User.objects.create_superuser(
-        username="admin",
-        email="admin@farmacialalysalma.co.mz",
-        password="Admin@123"
+        username='admin',
+        email='admin@farmacialalysalma.co.mz',
+        password='Admin@123'
     )
-    return HttpResponse("✅ Admin criado com sucesso")
-    
-from django.core.management import call_command
-from django.http import HttpResponse
+    return HttpResponse("Admin criado com sucesso")
 
-def executar_migracoes(request):
-    call_command("migrate")
-    return HttpResponse("Migrações executadas com sucesso")
