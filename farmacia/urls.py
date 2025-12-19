77da516bae531a3db1368c27b632b7256ae33429
farmacialalysalma01-lang <farmacialalysalma01@gmail.com>
2025-12-20 00:35:59 +0200

    Add executar-migracoes route

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -1,9 +1,8 @@
-from .views import executar_migracoes
 from django.urls import path
-from .views import home, criar_admin
+from .views import home, criar_admin, executar_migracoes
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("criar-admin/", criar_admin),
-    path("executar-migracoes/", executar_migracoes)
+    path('', home, name='home'),
+    path('criar-admin/', criar_admin),
+    path('executar-migracoes/', executar_migracoes),
 ]
