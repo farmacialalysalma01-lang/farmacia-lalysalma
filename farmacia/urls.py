2554d75e641c0a9f0c2a46ea065a3e3dc8fb29f2
farmacialalysalma01-lang <farmacialalysalma01@gmail.com>
2025-12-20 00:27:09 +0200

    Add temporary endpoint to run migrations

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -1,7 +1,9 @@
+from .views import executar_migracoes
 from django.urls import path
 from .views import home, criar_admin
 
 urlpatterns = [
     path("", home, name="home"),
     path("criar-admin/", criar_admin),
+    path("executar-migracoes/", executar_migracoes)
 ]
