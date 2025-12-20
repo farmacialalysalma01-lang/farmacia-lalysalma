9c8ec8e423c3afa87ac708bee60de6c6dce14515
farmacialalysalma01-lang <farmacialalysalma01@gmail.com>
2025-12-20 02:47:33 +0200

    Delete farmacia directory

--- farmacia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import criar_admin
-from .views import home, criar_admin, executar_migracoes
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('criar-admin/', criar_admin),
-    path('executar-migracoes/', executar_migracoes),
-
