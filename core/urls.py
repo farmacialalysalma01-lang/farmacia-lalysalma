7ab733aa57abc1d2a3d4ffc4d6cccf5dc4350e6c
ALGIK SISTEMA DE GESTAO DE FARMACIA <farmacialalysalma01@gmail.com>
2025-12-20 11:55:23 +0200

    Add login and logout routes

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from farmacia import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+
+    # LOGIN e LOGOUT
+    path('login/', auth_views.LoginView.as_view(
+        template_name='login.html'
+    ), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # ADMIN DJANGO
     path('admin/', admin.site.urls),
-    path('', include('farmacia.urls')),
 ]
 
