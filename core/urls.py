415ff37760778a76412656b42a84d62ad81e92e6
ALGIK SISTEMA DE GESTAO DE FARMACIA <farmacialalysalma01@gmail.com>
2025-12-20 12:22:50 +0200

    Create temporary admin user endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from farmacia import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', include('farmacia.urls')),
+    path('admin/',admin.site.urls),
 
     # LOGIN e LOGOUT
     path('login/', auth_views.LoginView.as_view(
