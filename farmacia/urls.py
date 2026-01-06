08d90843f3e11be440b05e69ed448ae2dd2377a3
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-07 01:42:01 +0200

    Update urls.py

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import home, login_view
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("login/", login_view, name="login"),
+    path("", views.home, name="home"),
     path("login/", views.login_view, name="login"),
 ]
-from . import views
