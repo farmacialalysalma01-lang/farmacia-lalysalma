9d61a45c664b0497249bcd6ae6f3d58205c75d52
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-07 00:59:21 +0200

    Improve admin listing for stock management

--- farmacia/admin.py
+++ farmacia/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Categoria, Medicamento
 
-admin.site.register(Categoria)
-admin.site.register(Medicamento)
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
+
+
+@admin.register(Medicamento)
+class MedicamentoAdmin(admin.ModelAdmin):
+    list_display = (
+        "nome",
+        "categoria",
+        "quantidade",
+        "stock_minimo",
+        "preco_venda",
+        "data_validade",
+    )
+
+    list_filter = ("categoria", "data_validade")
+
+    search_fields = ("nome",)
+
+    ordering = ("nome",)
