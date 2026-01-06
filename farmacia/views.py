9c8fa802630acdb0cbfe1ba0293d81368a4842f2
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-07 01:29:06 +0200

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -19,3 +19,22 @@ def login_view(request):
             messages.error(request, "Utilizador ou senha incorretos")
 
     return render(request, "login.html")
+    from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/")  # depois mudamos para dashboard
+        else:
+            messages.error(request, "Usuário ou senha incorretos")
+
+    return render(request, "login.html")
+
