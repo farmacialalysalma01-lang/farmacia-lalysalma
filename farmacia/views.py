cac4c41fac632e963048862e2e4004a2a40ba674
ALGIK SISTEMA DE GESTAO DE FARMACIA <farmacialalysalma01@gmail.com>
2026-01-06 13:07:24 -0800

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("FUNCIONA – Farmácia Lalysalma")
+    return HttpResponse("Sistema de Gestão da Farmácia Lalysalma – Online")
