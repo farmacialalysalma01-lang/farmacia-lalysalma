1722a3053fe51bb8202c7cababf6f36a10f2eba8
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-07 23:35:07 +0200

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -5,3 +5,10 @@ def home(request):
 
 def login_view(request):
     return render(request, "login.html")
+
+from django.http import HttpResponse
+from django.core.management import call_command
+
+def run_migrate(request):
+    call_command("migrate")
+    return HttpResponse("Migrações executadas com sucesso!")
