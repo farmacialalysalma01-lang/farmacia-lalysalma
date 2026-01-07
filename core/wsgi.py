c055630d32ccf213578232ff2637fbfca9f1dc71
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-08 01:36:17 +0200

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,6 +1,9 @@
-import os
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
+
+# Auto-migrate on startup
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error:", e)
