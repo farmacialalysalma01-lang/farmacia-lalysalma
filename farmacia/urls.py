6f7cf4d164dc2d955cca052c9f8925a9352c358d
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-07 02:32:01 +0200

    Update urls.py

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -5,3 +5,11 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login_view, name="login"),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("login/", views.login_view, name="login"),
+    path("run-migrate/", views.run_migrate),   # 🚀 NOVA ROTA
+]
