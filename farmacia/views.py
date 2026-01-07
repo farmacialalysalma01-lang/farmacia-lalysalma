ec6d079520dcff68475c526be2d8b5bea09120e0
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-07 22:57:27 +0200

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -5,14 +5,3 @@ def home(request):
 
 def login_view(request):
     return render(request, "login.html")
-
-from django.http import HttpResponse
-from django.core.management import call_command
-
-def criar_admin(request):
-    User.objects.create_superuser(
-        username="admin",
-        password="admin123",
-        email=""
-    )
-    return HttpResponse("Admin criado com sucesso!")
