f05573efc547ecd6c9af7d95691b73b6740f5a39
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-08 01:46:20 +0200

    Update models.py

--- farmacia/models.py
+++ farmacia/models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 
 class Medicamento(models.Model):
-    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=150)
     categoria = models.CharField(max_length=100, blank=True, null=True)
-    fornecedor = models.CharField(max_length=150, blank=True, null=True)
+    fornecedor = models.CharField(max_length=120, blank=True, null=True)
+
     quantidade = models.IntegerField(default=0)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    validade = models.DateField(blank=True, null=True)
+
+    preco_compra = models.DecimalField(max_digits=10, decimal_places=2)
+    preco_venda = models.DecimalField(max_digits=10, decimal_places=2)
+
+    data_validade = models.DateField(blank=True, null=True)
+    data_registro = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nome
+
+
+class Venda(models.Model):
+    medicamento = models.ForeignKey(Medicamento, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Venda de {self.medicamento.nome}"
