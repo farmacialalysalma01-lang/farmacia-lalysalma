7a17309ddaeca5f8ff44cf723c19048a706bac27
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-08 14:54:43 -0800

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -4,11 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-
-# Auto migrate in Render
-try:
-    from django.core.management import call_command
-    call_command("makemigrations", "farmacia")
-    call_command("migrate")
-except Exception as e:
-    print("Migration error:", e)
