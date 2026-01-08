a80b3b692b5c1d233408101c1517770c10a7059a
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-08 13:29:08 -0800

    Update admin.py

--- farmacia/admin.py
+++ farmacia/admin.py
@@ -1,13 +1,5 @@
 from django.contrib import admin
-from .models import Categoria, Medicamento
 from .models import Medicamento
-
-
-admin.site.register(Medicamento)
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ("nome",)
-    search_fields = ("nome",)
-
 
 @admin.register(Medicamento)
 class MedicamentoAdmin(admin.ModelAdmin):
@@ -15,20 +7,8 @@ class MedicamentoAdmin(admin.ModelAdmin):
         "nome",
         "categoria",
         "quantidade",
-        "stock_minimo",
         "preco_venda",
         "data_validade",
-        "status_stock",
     )
-
-    list_filter = ("categoria", "data_validade")
-
-    search_fields = ("nome",)
-
-    ordering = ("nome",)
-
-    def status_stock(self, obj):
-        return "⚠️ BAIXO" if obj.quantidade <= obj.stock_minimo else "OK"
-
-    status_stock.short_description = "Estado do Stock"
-
+    search_fields = ("nome", "categoria")
+    list_filter = ("categoria",)
