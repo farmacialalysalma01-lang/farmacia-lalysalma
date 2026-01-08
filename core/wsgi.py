01a279c5f74d1e1700d91649e2b7e7c0c91641ea
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-08 02:13:29 +0200

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -5,7 +5,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
 
-
 # Auto migrate in Render
 try:
     from django.core.management import call_command
