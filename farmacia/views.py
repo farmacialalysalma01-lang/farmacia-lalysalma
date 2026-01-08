06efee8710768d0d741e105a5f294f1536ef23f0
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-08 15:57:55 -0800

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -1,36 +1,4 @@
-from django.http import HttpResponse, HttpResponseForbidden
-from django.core.management import call_command
-from django.contrib.auth.models import User
+from django.shortcuts import render
 
-SETUP_KEY = "farmacia2026"  # 🔐 MUDE ESTA SENHA
-
-def setup_system(request):
-    key = request.GET.get("key")
-
-    if key != SETUP_KEY:
-        return HttpResponseForbidden("Acesso negado")
-
-    # Executar migrations
-    call_command("makemigrations")
-    call_command("migrate")
-
-    # Criar superuser se não existir
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@farmacia.com",
-            password="admin123"
-        )
-
-    return HttpResponse("Sistema configurado com sucesso!")
-
-
-    # criar admin se não existir
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@farmacia.com",
-            password="admin123"
-        )
-
-    return HttpResponse("Sistema configurado com sucesso!")
+def home(request):
+    return render(request, "home.html")
