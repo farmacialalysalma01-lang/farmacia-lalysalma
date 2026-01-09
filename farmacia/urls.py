44f677867a877e41dcbd4ae691453fea6eb227a7
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 01:13:32 +0200

    Fix login_view error causing deploy failure

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -1,8 +1,5 @@
-from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("login/", views.login_view, name="login"),
-    path("run-migrate/", views.run_migrate),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
 ]
