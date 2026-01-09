532230b8486471f83f74acbbc495f7de2ce254f3
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-08 16:08:12 -0800

    Register Medicamento in admin

--- farmacia/admin.py
+++ farmacia/admin.py
@@ -3,12 +3,6 @@ from .models import Medicamento
 
 @admin.register(Medicamento)
 class MedicamentoAdmin(admin.ModelAdmin):
-    list_display = (
-        "nome",
-        "categoria",
-        "quantidade",
-        "preco_venda",
-        "data_validade",
-    )
+    list_display = ("nome", "categoria", "quantidade", "preco_venda", "data_validade")
     search_fields = ("nome", "categoria")
     list_filter = ("categoria",)
