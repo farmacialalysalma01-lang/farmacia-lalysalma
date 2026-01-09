d68993046acbaecd1ba575c4c0b091f8bbdeb604
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-09 23:59:00 +0200

    Update apps.py

--- farmacia/apps.py
+++ farmacia/apps.py
@@ -2,5 +2,19 @@ from django.apps import AppConfig
 
 
 class FarmaciaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'farmacia'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "farmacia"
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        username = "admin"
+        password = "Admin@2026"
+        email = "admin@farmacia.com"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
