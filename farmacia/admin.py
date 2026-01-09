51dc93e9abe998621ae72046b2e404110e219d98
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 00:17:48 +0200

    "Add stock entry and exit system"

--- farmacia/admin.py
+++ farmacia/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Medicamento
+from .models import Medicamento, MovimentoStock
+
 
 @admin.register(Medicamento)
 class MedicamentoAdmin(admin.ModelAdmin):
-    list_display = (
-        "nome",
-        "quantidade",
-        "preco_venda",
-        "data_validade",
-    )
+    list_display = ("nome", "preco", "quantidade", "data_criacao")
     search_fields = ("nome",)
-    list_filter = ("data_validade",)
+
+
+@admin.register(MovimentoStock)
+class MovimentoStockAdmin(admin.ModelAdmin):
+    list_display = ("medicamento", "tipo", "quantidade", "data")
+    list_filter = ("tipo", "data")
+    search_fields = ("medicamento__nome",)
