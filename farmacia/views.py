d324a38b646efe3f1523e48e922f8017f498ad01
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 01:07:27 +0200

    Fix views: separate home and run_migrate correctly

--- farmacia/views.py
+++ farmacia/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.core.management import call_command
+
 
 def run_migrate(request):
     if request.GET.get("key") != "farmacia2026":
@@ -9,3 +11,7 @@ def run_migrate(request):
     call_command("migrate")
 
     return HttpResponse("Migrations executadas com sucesso!")
+
+
+def home(request):
+    return HttpResponse("Sistema da Farmácia Lalysalma está ativo.")
