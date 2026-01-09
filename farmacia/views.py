00730ea164d20327a0df3c87e33845e724b44974
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-09 23:42:42 +0200

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
-def home(request):
-    return render(request, "home.html")
+def criar_admin(request):
+    if User.objects.filter(username="admin").exists():
+        return HttpResponse("Admin já existe")
 
-@login_required
-def dashboard(request):
-    return render(request, "dashboard.html")
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@farmacialalysalma.co.mz",
+        password="Admin@2026"
+    )
+    return HttpResponse("Admin criado com sucesso")
