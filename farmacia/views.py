a17708eb09e0fb27c76b31d220cd57cc4f7b7c4c
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 21:06:12 +0200

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "home.html")
+
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def setup_admin(request):
+    if User.objects.filter(username="admin").exists():
+        return HttpResponse("Admin já existe")
+
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@farmacia.com",
+        password="Admin@2026"
+    )
+    return HttpResponse("Admin criado com sucesso")
+
