cd0bde563dc46dd4aa753bfaf5f563a8feffdfd2
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 03:15:29 +0200

    Update admin.py

--- farmacia/admin.py
+++ farmacia/admin.py
@@ -4,7 +4,7 @@ from .models import Medicamento, MovimentoStock
 
 @admin.register(Medicamento)
 class MedicamentoAdmin(admin.ModelAdmin):
-    list_display = ("nome", "preco", "quantidade")
+    list_display = ("nome", "preco", "quantidade", "data_criacao")
     search_fields = ("nome",)
 
 
@@ -13,14 +13,3 @@ class MovimentoStockAdmin(admin.ModelAdmin):
     list_display = ("medicamento", "tipo", "quantidade", "data")
     list_filter = ("tipo", "data")
     search_fields = ("medicamento__nome",)
-
-from .models import Venda, ItemVenda
-
-class ItemVendaInline(admin.TabularInline):
-    model = ItemVenda
-    extra = 1
-
-@admin.register(Venda)
-class VendaAdmin(admin.ModelAdmin):
-    inlines = [ItemVendaInline]
-    list_display = ("id", "data", "total")
