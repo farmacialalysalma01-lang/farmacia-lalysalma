d18895c66544964720d0389092fa345ed52c70c9
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 21:12:59 +0200

    Update urls.py

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -1,6 +1,8 @@
 from .vews import setup_admin
+from .views import login_view
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("setup/", setup_admin),
+    path("login/", login_view),
 ]
