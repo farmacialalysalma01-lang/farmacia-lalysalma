07dc4daa934940970cd9736e120ca710c49ce96b
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 03:02:12 +0200

    Update urls.py

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login_view, name="login"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("recibo/<int:venda_id>/", views.recibo_pdf, name="recibo_pdf"),
+    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
