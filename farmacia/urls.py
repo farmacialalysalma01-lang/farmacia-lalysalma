8d285c2987aa6f720a82beb2b080defc79935635
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 03:00:05 +0200

    Update urls.py

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login_view, name="login"),
     path("dashboard/", views.dashboard, name="dashboard"),
+    path("recibo/<int:venda_id>/", views.recibo_pdf, name="recibo_pdf"),
 ]
