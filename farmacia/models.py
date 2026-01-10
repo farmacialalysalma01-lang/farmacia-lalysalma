9f74965fba95600d269c9ff37da32d9711b22a73
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 23:22:58 +0200

    Update models.py

--- farmacia/models.py
+++ farmacia/models.py
@@ -29,3 +29,36 @@ class SaidaStock(models.Model):
 
     def __str__(self):
         return f"Venda {self.produto.nome}"
+
+from django.contrib.auth.models import User
+
+class Venda(models.Model):
+    produto = models.ForeignKey("Produto", on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+    preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    cliente = models.CharField(max_length=100, blank=True)
+    forma_pagamento = models.CharField(
+        max_length=20,
+        choices=[
+            ("Dinheiro", "Dinheiro"),
+            ("M-Pesa", "M-Pesa"),
+            ("E-Mola", "E-Mola"),
+            ("Cartão", "Cartão"),
+        ],
+    )
+    data = models.DateTimeField(auto_now_add=True)
+    operador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def save(self, *args, **kwargs):
+        self.total = self.quantidade * self.preco_unitario
+
+        # baixa o stock
+        self.produto.stock -= self.quantidade
+        self.produto.save()
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.produto.nome} - {self.quantidade} unid"
+
