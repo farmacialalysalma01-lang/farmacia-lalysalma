75c334d4fb3dc56c3b58afc915a2828ecf777c5d
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-09 16:40:21 -0800

    Update models.py

--- farmacia/models.py
+++ farmacia/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 
-
 class Medicamento(models.Model):
-    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=100)
     descricao = models.TextField(blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    quantidade = models.PositiveIntegerField(default=0)
+    quantidade = models.integerField()
     data_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nome
-
 
 class MovimentoStock(models.Model):
     TIPO_CHOICES = (
