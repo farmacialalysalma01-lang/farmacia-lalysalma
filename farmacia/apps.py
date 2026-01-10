e6472148bb277a71594edc7f56b8b4fe509af9b1
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-10 22:07:57 +0200

    Update apps.py

--- farmacia/apps.py
+++ farmacia/apps.py
@@ -4,3 +4,16 @@ class FarmaciaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "farmacia"
 
+    def ready(self):
+        from django.contrib.auth.models import User
+        from django.db.utils import OperationalError
+
+        try:
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    password="Admin@2026",
+                    email="admin@farmacialalysalma.com"
+                )
+        except OperationalError:
+            pass
