49a17dadd7c8ddaa640893a8939f15bd8709f233
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-09 16:02:03 -0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('farmacia.urls')),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls'),
+    path("", include("farmacia.urls")),
 ]
