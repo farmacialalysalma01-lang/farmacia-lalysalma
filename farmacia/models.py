ebb13c5ae3af1f45d227764633a1ededbedf04d3
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-12 01:51:20 +0200

    Update models.py

--- farmacia/models.py
+++ farmacia/models.py
@@ -36,8 +36,10 @@ class Venda(models.Model):
     produto = models.ForeignKey("Produto", on_delete=models.CASCADE)
     quantidade = models.PositiveIntegerField()
     preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
-    total = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    
     cliente = models.CharField(max_length=100, blank=True)
+    
     forma_pagamento = models.CharField(
         max_length=20,
         choices=[
@@ -45,13 +47,14 @@ class Venda(models.Model):
             ("M-Pesa", "M-Pesa"),
             ("E-Mola", "E-Mola"),
             ("Cartão", "Cartão"),
-        ],
+        ]
     )
-    data = models.DateTimeField(auto_now_add=True)
-    operador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
-    def save(self, *args, **kwargs):
-        self.total = self.quantidade * self.preco_unitario
+    operador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Venda de {self.produto.nome} - {self.quantidade}"
 
         # baixa o stock
         self.produto.stock -= self.quantidade
