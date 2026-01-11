953c3c3a7eb19dd9e608f69b11898e0c0a41f8fc
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-11 22:56:49 +0200

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -1,41 +1,53 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+        username = request.POST["username"]
+        password = request.POST["password"]
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
 
-            # Redireciona conforme grupo
-            if user.groups.filter(name="CAIXA").exists():
-                return redirect("/caixa/")
-            elif user.groups.filter(name="FARMACEUTICO").exists():
-                return redirect("/farmaceutico/")
-            elif user.groups.filter(name="GERENTE").exists():
-                return redirect("/gerente/")
-            else:
+            if user.is_superuser:
                 return redirect("/admin/")
 
-        return render(request, "login.html", {"error": "Credenciais inválidas"})
+            if user.groups.filter(name="CAIXA").exists():
+                return redirect("/caixa/")
+
+            if user.groups.filter(name="FARMACEUTICO").exists():
+                return redirect("/farmaceutico/")
+
+            if user.groups.filter(name="GERENTE").exists():
+                return redirect("/gerente/")
+
+            return redirect("/")
+
+        else:
+            return render(request, "login.html", {"error": "Login inválido"})
 
     return render(request, "login.html")
 
-from django.contrib.auth.decorators import login_required
 
 @login_required
-def caixa_dashboard(request):
+def caixa(request):
     return render(request, "caixa.html")
 
-@login_required
-def farmaceutico_dashboard(request):
-    return render(request, "farmaceutico.html")
 
 @login_required
-def gerente_dashboard(request):
+def farmaceutico(request):
+    return render(request, "farmaceutico.html")
+
+
+@login_required
+def gerente(request):
     return render(request, "gerente.html")
+
+
+@login_required
+def home(request):
+    return render(request, "home.html")
