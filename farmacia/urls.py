e77c4fb48e92b3aa49cd5032343ad4a9c7ca1bb0
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-11 17:25:05 +0200

    Update urls.py

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -3,8 +3,4 @@ from . import views
 
 urlpatterns = [
     path("", views.login_view, name="login"),
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("caixa/", views.caixa, name="caixa"),
-    path("farmaceutico/", views.farmaceutico, name="farmaceutico"),
-    path("gerente/", views.gerente, name="gerente"),
 ]
