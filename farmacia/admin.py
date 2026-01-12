e411450b5a93edcb3f924c3cbf07b1bd1649de16
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-12 05:16:02 +0200

    Update admin.py

--- farmacia/admin.py
+++ farmacia/admin.py
@@ -1,22 +1,33 @@
 from django.contrib import admin
-from .models import Produto, Venda, EntradaStock, SaidaStock
+from .models import Produto, EntradaStock, SaidaStock, Venda
+
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ("nome", "preco_venda", "stock")
+    list_display = ("id", "nome", "preco", "stock")
     search_fields = ("nome",)
-    list_filter = ("nome",)
 
-@admin.register(Venda)
-class VendaAdmin(admin.ModelAdmin):
-    list_display = ("id", "produto", "quantidade", "total", "forma_pagamento", "data")
-    list_filter = ("forma_pagamento", "data")
-    search_fields = ("produto__nome",)
 
 @admin.register(EntradaStock)
 class EntradaStockAdmin(admin.ModelAdmin):
     list_display = ("produto", "quantidade", "data")
 
+
 @admin.register(SaidaStock)
 class SaidaStockAdmin(admin.ModelAdmin):
     list_display = ("produto", "quantidade", "data")
+
+
+@admin.register(Venda)
+class VendaAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "produto",
+        "quantidade",
+        "preco_unitario",
+        "total",
+        "forma_pagamento",
+        "operador",
+        "data",
+    )
+    list_filter = ("forma_pagamento", "data")
