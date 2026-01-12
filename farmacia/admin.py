84ca9263b9a1b7ca18f550193c34cb60200f5b8a
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-13 01:41:44 +0200

    Update admin.py

--- farmacia/admin.py
+++ farmacia/admin.py
@@ -1,40 +1,5 @@
-from .admin_dashboard import dashboard_data
 from django.contrib import admin
-from .models import Produto, EntradaStock, SaidaStock, Venda
+from .models import Produto, Venda
 
-
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ("id", "nome", "preco", "stock")
-    search_fields = ("nome",)
-
-
-@admin.register(EntradaStock)
-class EntradaStockAdmin(admin.ModelAdmin):
-    list_display = ("produto", "quantidade", "data")
-
-
-@admin.register(SaidaStock)
-class SaidaStockAdmin(admin.ModelAdmin):
-    list_display = ("produto", "quantidade", "data")
-
-
-@admin.register(Venda)
-class VendaAdmin(admin.ModelAdmin):
-    list_display = ("id", "total", "forma_pagamento", "operador", "data")
-
-
-from django.contrib.admin import AdminSite
-
-class AlgikAdminSite(AdminSite):
-    site_header = "ALGIK – Farmácia Lalysalma"
-    site_title = "ALGIK Admin"
-    index_title = "Painel Executivo"
-
-    def each_context(self, request):
-        context = super().each_context(request)
-        context.update(dashboard_data(request))
-        return context
-
-algik_admin = AlgikAdminSite(name="algik_admin")
-
+admin.site.register(Produto)
+admin.site.register(Venda)
