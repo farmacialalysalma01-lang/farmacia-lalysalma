1ab424fb05b38bead268c899fdbe70237c69f445
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-12 03:48:36 +0200

    Update models.py

--- farmacia/models.py
+++ farmacia/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Produto(models.Model):
     nome = models.CharField(max_length=200)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
@@ -9,26 +8,6 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.nome
-
-
-class EntradaStock(models.Model):
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    quantidade = models.PositiveIntegerField()
-    fornecedor = models.CharField(max_length=200, blank=True)
-    data = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Entrada - {self.produto.nome}"
-
-
-class SaidaStock(models.Model):
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    quantidade = models.PositiveIntegerField()
-    motivo = models.CharField(max_length=200, blank=True)
-    data = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Saída - {self.produto.nome}"
 
 
 class Venda(models.Model):
@@ -45,7 +24,7 @@ class Venda(models.Model):
             ("M-Pesa", "M-Pesa"),
             ("E-Mola", "E-Mola"),
             ("Cartão", "Cartão"),
-        ],
+        ]
     )
 
     operador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
