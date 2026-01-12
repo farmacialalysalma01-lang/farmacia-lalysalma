15c1b32f0ea345634660ecc292ba412799425489
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-12 04:02:09 +0200

    Update admin.py

--- farmacia/admin.py
+++ farmacia/admin.py
@@ -5,32 +5,19 @@ from .models import Produto, EntradaStock, SaidaStock, Venda
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ("nome", "preco", "stock")
-    search_fields = ("nome",)
 
 
 @admin.register(EntradaStock)
 class EntradaStockAdmin(admin.ModelAdmin):
-    list_display = ("produto", "quantidade", "fornecedor", "data")
-    list_filter = ("data",)
+    list_display = ("produto", "quantidade", "data")
 
 
 @admin.register(SaidaStock)
 class SaidaStockAdmin(admin.ModelAdmin):
-    list_display = ("produto", "quantidade", "motivo", "data")
-    list_filter = ("data",)
+    list_display = ("produto", "quantidade", "data")
 
 
 @admin.register(Venda)
 class VendaAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "produto",
-        "quantidade",
-        "preco_unitario",
-        "total",
-        "forma_pagamento",
-        "operador",
-        "data",
-    )
+    list_display = ("id", "produto", "quantidade", "total", "forma_pagamento", "operador", "data")
     list_filter = ("forma_pagamento", "data")
-    search_fields = ("produto__nome", "cliente")
