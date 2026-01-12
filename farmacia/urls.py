6d9e38e760e47b22b5c2fa3480802f2822496e13
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-12 03:14:27 +0200

    Update urls.py

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
 
-    path("caixa/", views.caixa_dashboard, name="caixa"),
+    path("caixa/", views.caixa_home, name="caixa"),
     path("caixa/nova-venda/", views.nova_venda, name="nova_venda"),
-    path("caixa/historico/", views.historico_vendas, name="historico_vendas"),
-    path("caixa/recibo/<int:venda_id>/", views.emitir_recibo, name="emitir_recibo"),
-]
+    path("caixa/historico/", views.historico_vendas, name="historico"),
+    path("caixa/recibo/<int:venda_id>/", views.emitir_recibo, name="recibo"),
 
+    path("logout/", views.logout_view, name="logout"),
+]
