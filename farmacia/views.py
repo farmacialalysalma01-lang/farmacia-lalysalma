0a9a22244116f33ec14a6dadde30287cc16e1cda
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-13 02:36:23 +0200

    Update views.py

--- farmacia/views.py
+++ farmacia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from . import models
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.utils.timezone import now
