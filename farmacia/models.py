b71a9c8f8811e45ffbcde24dd1ab1c6be01d2805
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-14 00:02:56 +0200

    Update models.py

--- farmacia/models.py
+++ farmacia/models.py
@@ -10,14 +10,15 @@ class Produto(models.Model):
         return self.nome
 
 
+from django.db import models
+
 class Venda(models.Model):
     data = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(max_digits=10, decimal_places=2)
-    pagamento = models.CharField(max_length=50)
-    operador = models.ForeignKey(User, on_delete=models.CASCADE)
+    pagamento = models.CharField(max_length=30)
 
     def __str__(self):
-        return f"Venda {self.id} - {self.total}"
+        return f"Venda {self.id} - {self.total} MZN"
 
 
 class ItemVenda(models.Model):
