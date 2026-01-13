5ad33bf6856dac887c590ef04b0d6a00943c09b4
farmacialalysalma01 <farmacialalysalma01@gmail.com>
2026-01-13 21:04:56 +0200

    Fix urls

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.login_view, name="login"),
+    path("login/", views.login_view),
+    path("logout/", views.logout_view),
+
     path("caixa/", views.area_caixa),
     path("nova-venda/", views.nova_venda),
     path("finalizar-venda/", views.finalizar_venda),
